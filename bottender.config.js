9a282e07368e864af27aab63adc9658a80c90e11
Viet Hoang <ngviethoang0212@gmail.com>
2023-02-21 21:14:49 +0700

    update cr

--- bottender.config.js
+++ bottender.config.js
@@ -32,7 +32,14 @@ module.exports = {
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
       verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
-      fields: ['messages', 'messaging_postbacks', 'messaging_optins', 'messaging_referrals', 'messaging_handovers', 'messaging_policy_enforcement'],
+      fields: [
+        'messages',
+        'messaging_postbacks',
+        'messaging_optins',
+        'messaging_referrals',
+        'messaging_handovers',
+        'messaging_policy_enforcement',
+      ],
       profile: {
         getStarted: {
           payload: 'GET_STARTED',
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const SERVICES = [
     name: 'Open Journey',
     version: '9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb',
   },
-]
+];
 
 async function postPrediction(version, input) {
   const response = await axios({
@@ -18,7 +18,7 @@ async function postPrediction(version, input) {
     url: 'https://api.replicate.com/v1/predictions',
     headers: {
       Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     data: {
       version,
@@ -27,7 +27,7 @@ async function postPrediction(version, input) {
   });
 
   if (response.status !== 201) {
-    console.error(response.data)
+    console.error(response.data);
     return null;
   }
 
@@ -37,17 +37,17 @@ async function postPrediction(version, input) {
 async function getPrediction(id) {
   const response = await axios({
     method: 'GET',
-    url: "https://api.replicate.com/v1/predictions/" + id,
+    url: 'https://api.replicate.com/v1/predictions/' + id,
     headers: {
       Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
   if (response.status !== 200) {
-    console.error(response.data)
+    console.error(response.data);
     return null;
   }
-  return response.data
+  return response.data;
 }
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
@@ -58,51 +58,55 @@ async function Command(
     match: {
       groups: { command, content },
     },
-  },
+  }
 ) {
   // await context.sendText(`Executing command ${command} with content: ${content}`);
   switch (command) {
     case 'h':
     case 'help':
       await context.sendText('Here is the list of commands\n1.');
-      break
+      break;
     case 's':
     case 'service':
       await context.sendText('Select a service:', {
         quickReplies: SERVICES.map((service, i) => ({
-            contentType: 'text',
-            title: service.name,
-            payload: `s_${i}`,
-          })),
+          contentType: 'text',
+          title: service.name,
+          payload: `s_${i}`,
+        })),
       });
-      break
+      break;
     default:
       await context.sendText('Sorry. Command not found.');
-      break
+      break;
   }
 }
 
 async function Others(context, props) {
   // await context.sendText('Sorry. I do not understand what you say.');
   return;
-  let prediction = await postPrediction('f178fa7a1ae43a9a9af01b833b9d2ecf97b1bcb0acfd2dc5dd04895e042863f1', { prompt: context.event.text })
+  let prediction = await postPrediction(
+    'f178fa7a1ae43a9a9af01b833b9d2ecf97b1bcb0acfd2dc5dd04895e042863f1',
+    { prompt: context.event.text }
+  );
   if (!prediction) {
     await context.sendText('Error');
     return;
   }
 
   while (
-      prediction && prediction.status !== 'succeeded' &&
-      prediction.status !== 'failed'
+    prediction &&
+    prediction.status !== 'succeeded' &&
+    prediction.status !== 'failed'
   ) {
-      await context.sendText(prediction.status);
-      await sleep(3000);
-      const response = await getPrediction(prediction.id);
-      if (!response) {
-        await context.sendText('Error getting prediction');
-        return;
-      }
-      prediction = response;
+    await context.sendText(prediction.status);
+    await sleep(3000);
+    const response = await getPrediction(prediction.id);
+    if (!response) {
+      await context.sendText('Error getting prediction');
+      return;
+    }
+    prediction = response;
   }
   for (const image of prediction.output) {
     await context.sendImage(image);
@@ -110,7 +114,7 @@ async function Others(context, props) {
 }
 
 async function Payload(context, props) {
-  console.log(context.event.payload)
+  console.log(context.event.payload);
 }
 
 module.exports = async function App(context, props) {
@@ -120,4 +124,4 @@ module.exports = async function App(context, props) {
     text(/^\/(?<command>\w+)(?:\s(?<content>.+))?/i, Command),
     text('*', Others),
   ]);
-}
+};
